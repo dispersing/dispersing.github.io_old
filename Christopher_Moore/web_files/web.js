4eb17484991ffee726a9f98abc4d17ab625e7f6d
Christopher Moore <life.dispersing@gmail.com>
2014-09-21 13:12:52 -0400

    iWeb sync

---
+++ Christopher_Moore/web_files/web.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20130617
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWPhotoFrame([IWCreateImage('web_files/Formal_inset_01.png'),IWCreateImage('web_files/Formal_inset_02.png'),IWCreateImage('web_files/Formal_inset_03.png'),IWCreateImage('web_files/Formal_inset_06.png'),IWCreateImage('web_files/Formal_inset_09.png'),IWCreateImage('web_files/Formal_inset_08.png'),IWCreateImage('web_files/Formal_inset_07.png'),IWCreateImage('web_files/Formal_inset_04.png')],null,0,0.600000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('web_files/webMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
