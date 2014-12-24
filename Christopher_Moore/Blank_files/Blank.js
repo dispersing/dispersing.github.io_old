620204696c1938daf889ccc98196a65e40e1ac1e
Christopher Moore <life.dispersing@gmail.com>
2014-12-24 08:17:09 -0800

    Updated map
    From Cluster to RevolutionMaps

--- Christopher_Moore/Blank_files/Blank.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20141126
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Blank_files/BlankMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
