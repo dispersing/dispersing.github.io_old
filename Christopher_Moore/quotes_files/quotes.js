8864cc70dc37a6c4834ee53cb9b9cf3e6bcc729e
Christopher Moore <life.dispersing@gmail.com>
2015-10-03 12:25:40 -0400

    More to Jekyll

--- Christopher_Moore/quotes_files/quotes.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20140921
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('quotes_files/Formal_inset_01.png'),IWCreateImage('quotes_files/Formal_inset_02.png'),IWCreateImage('quotes_files/Formal_inset_03.png'),IWCreateImage('quotes_files/Formal_inset_06.png'),IWCreateImage('quotes_files/Formal_inset_09.png'),IWCreateImage('quotes_files/Formal_inset_08.png'),IWCreateImage('quotes_files/Formal_inset_07.png'),IWCreateImage('quotes_files/Formal_inset_04.png')],null,0,0.600000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000)});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('quotes_files/quotesMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
